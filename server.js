151a1f4feb82898f20508b83d40a61d2157ca7cb
Your Name <you@example.com>
2017-01-13 12:11:57 -0300

    corrigiendo error en server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.set.("port", (process.env.PORT || 8086));
+app.set("port", (process.env.PORT || 8086));
 app.use(express.static(__dirname + "/dist"));
 
 app.listen(app.get("port"), function(){
